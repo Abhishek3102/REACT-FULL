c3564a7fb3e9ecaa92a613d9a88a538c6858d8b0
Abhishek3102 <ankushpan694@gmail.com>
2025-05-31 12:46:04 +0530

    feat(context): add user context with username and password state

--- First/src/Button.jsx
+++ First/src/Button.jsx
@@ -2,7 +2,7 @@ import { useState } from "react";
 function Button(props) {
  
     return(
-    <button onClick={props.handleClick}>Click Me</button>
+    <button className="click-button" onClick={props.handleClick}>Click Me</button>
     )
 }
 
